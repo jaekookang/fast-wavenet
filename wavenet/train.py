947561d268ffecd924e6bb614d8f030e07e08551
Jaekoo Kang <jaekoo.jk@gmail.com>
2020-05-10 12:33:03 -0400

    Create train.py

---
+++ wavenet/train.py
@@ -0,0 +1,42 @@
+import os
+from time import time
+from IPython.display import Audio
+from utils import make_batch
+from models import Model, Generator
+import tensorflow as tf
+
+tf.compat.v1.disable_eager_execution()
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+
+if __name__ == '__main__':
+    inputs, targets = make_batch('../assets/voice.wav')
+    num_time_samples = inputs.shape[1]
+    num_channels = 1
+    gpu_fraction = 1.0
+
+    print(inputs.shape, targets.shape)
+
+    model = Model(num_time_samples=num_time_samples,
+                  num_channels=num_channels,
+                  gpu_fraction=gpu_fraction)
+
+    try:
+        tic = time()
+        model.train(inputs, targets)
+        toc = time()
+    except:
+        print(' +++++ Stopped +++++ ')
+    finally:
+        print('Training took {} seconds.'.format(toc-tic))
+        
+        generator = Generator(model)
+
+        # Get first sample of input
+        input_ = inputs[:, 0:1, 0]
+
+        tic = time()
+        predictions = generator.run(input_, 32000, './', 44100)
+        toc = time()
+        print('Generating took {} seconds.'.format(toc-tic))
